55d326a541cd3115f5b6ba0f47ba98cdf4ddaa0d
André Santana <109093157+AndreSanLopes@users.noreply.github.com>
2026-05-23 20:35:45 -0300

    Primeiros passos com biblioteca JavaX

---
+++ aulasJava/src/praticaTelas.java
@@ -0,0 +1,11 @@
+//EXERCÍCIO PRÁTICO DE TELAS EM JAVA
+import javax.swing.*;
+
+public class praticaTelas {
+    public static void main(String[] args){
+    JFrame janela = new JFrame("Minha primeira tela");
+    janela.setSize(400, 300);
+    janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    janela.setVisible(true);
+    }
+}
