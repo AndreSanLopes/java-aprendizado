126649d816357b4963ab5c909daf5b4e731befd1
André Santana <109093157+AndreSanLopes@users.noreply.github.com>
2026-05-23 22:05:58 -0300

    Interface com Java Swing

---
+++ javaSwing/PrimeiraInterface/src/primeirainterface/PrimeiraInterface.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package primeirainterface;
+
+/**
+ *
+ * @author andre
+ */
+public class PrimeiraInterface {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
